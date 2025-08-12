3d7e474d06a232c93a47133366055c45ffbefa3b
Mark <brandonnarciso24@gmail.com>
2025-08-12 01:51:20 -0700

    fix(db): fix but when user reloads adds same item

--- src/controllers/addItemController.ts
+++ src/controllers/addItemController.ts
@@ -1,10 +1,9 @@
 import { Request, Response } from "express";
-import { postNewItem, getAllItems } from "../db/queries";
+import { postNewItem } from "../db/queries";
 
 export async function addNewItemToDB(req: Request, res: Response) {
   await postNewItem(req.body);
-  const items = await getAllItems();
-  res.render("index", { title: "Items list", items: items });
+  res.redirect("/");
 }
 
 export async function addItemForm(req: Request, res: Response) {
